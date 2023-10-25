de0d2a8853e9452a60625d053e705ba527e5ac2d
Ashley Milton <a.milton@unsw.edu.au>
2023-10-25 14:27:02 +1100

    Updates to 7

--- 7_methylKit_generic.R
+++ 7_methylKit_generic.R
@@ -32,9 +32,6 @@ filtered.text=filterByCoverage(all.samples, lo.perc = NULL, hi.count = NULL, hi.
 normalised.reads=normalizeCoverage(filtered.text)
 
 meth=methylKit::unite(normalised.reads, destrand = T) #unites data
-head(meth)
-
-unique(meth$chr)
 
 #### Putting split up chromosomes back together ####
 
@@ -46,19 +43,58 @@ meth$end <- ifelse(meth$chr=="chr1.B", meth$end+400000000, meth$end)
 meth$start <- ifelse(meth$chr=="chr2.B", meth$start+300000000, meth$start)
 meth$end <- ifelse(meth$chr=="chr2.B", meth$end+300000000, meth$end)
 
-# Filter for a specific chromosome pattern - checking to see that additions worked, and everything is as it should be
-filtered_df <- meth[grepl("^chr2$", meth$chr), ]
-
-# Get the range of values for the 'start' column
-start_range <- range(filtered_df$start)
-
-# Print the range
-print(start_range)
-
 #Second: adjust chr names
 meth$chr <- gsub('chr1.B', 'chr1', meth$chr)
 meth$chr <- gsub('chr2.B', 'chr2', meth$chr)
 unique(meth$chr)
+
+#### Creating the summary Cs and Ts dataframe ####
+
+methdf <- getData(meth)
+methdf$c <- ifelse(methdf$chr == "chrX", "X", "A")
+
+#Summing total numCs and numTs for each cell type
+summary <- methdf %>% group_by(c) %>% summarise(numCs_spg1 = sum(numCs1),
+                   numTs_spg1 = sum(numTs1),
+                   numCs_pd1 = sum(numCs2),
+                   numTs_pd1 = sum(numTs2),
+                   numCs_rs1 = sum(numCs3),
+                   numTs_rs1 = sum(numTs3),
+                   numCs_sperm1 = sum(numCs4),
+                   numTs_sperm1 = sum(numTs4),
+                   numCs_spg2 = sum(numCs5),
+                   numTs_spg2 = sum(numTs5),
+                   numCs_pd2 = sum(numCs6),
+                   numTs_pd2 = sum(numTs6),
+                   numCs_rs2 = sum(numCs7),
+                   numTs_rs2 = sum(numTs7),
+                   numCs_sperm2 = sum(numCs8),
+                   numTs_sperm2 = sum(numTs8)) %>% 
+  pivot_longer(2:17)
+  
+summary <- summary %>% separate(name, into = c("type", "name"), sep = "_") %>% pivot_wider(names_from = "type", values_from = "value") %>% mutate(percmeth = numCs/(numCs+numTs))
+
+summary$name <- gsub('spg1', 'Spg 1', summary$name)
+summary$name <- gsub('pd1', 'P/D 1', summary$name)
+summary$name <- gsub('rs1', 'RS 1', summary$name)
+summary$name <- gsub('sperm1', 'Sperm 1', summary$name)
+summary$name <- gsub('spg2', 'Spg 2', summary$name)
+summary$name <- gsub('pd2', 'P/D 2', summary$name)
+summary$name <- gsub('rs2', 'RS 2', summary$name)
+summary$name <- gsub('sperm2', 'Sperm 2', summary$name)
+
+write.table(summary, file="/path/to/out/dir/methylKit_summary_meth.txt", sep = "\t", quote = F, col.names = T, row.names = F)
+
+#Plot
+
+summary %>% ggplot(aes(x = c, y = percmeth, fill=c))+
+  geom_col()+
+  facet_wrap(~factor(name, levels=c("Spg 1", "P/D 1", "RS 1", "Sperm 1", "Spg 2", "P/D 2", "RS 2", "Sperm 2")), nrow = 1)+
+  theme_classic()+
+  scale_fill_manual(values = c("#cdccff", "#3e68db"), name = "", labels = c("Autosomes", "X chromosome"))+
+  theme(axis.title.x = element_blank())
+
+ggsave("/path/to/out/dir/methylKit_summary_meth.pdf", dpi=800, width = 10, height = 2.5, units = "in")
 
 #### Creating the percent methylation dataframe ####
 
@@ -108,106 +144,3 @@ head(percMeth2)
 
 #Saving the dataframe
 write.table(percMeth2, file = "/path/to/out/dir/Tammar_final_MEUv7_PercMeth_mincov1_150623.txt", quote = F, row.names = F, sep = "\t", col.names = T)
-
-
-#### Sequencing stats ####
-
-##Coverage histograms
-
-#Make smaller dataframe
-chr <- meth$chr
-start <- meth$start
-end <- meth$end
-spg1 <- meth$coverage1
-pd1 <- meth$coverage2
-rs1 <- meth$coverage3
-sperm1 <- meth$coverage4
-spg2 <- meth$coverage5
-pd2 <- meth$coverage6
-rs2 <- meth$coverage7
-sperm2 <- meth$coverage8
-coverage <- data.frame(chr, start, end, spg1, pd1, rs1, sperm1, spg2, pd2, rs2, sperm2)
-
-#Make it long format
-covlong <- pivot_longer(coverage, 4:11)
-
-#Assign colours to samples
-colours <- data.frame(
-  name = c("spg1", "pd1", "rs1", "sperm1", "spg2", "pd2", "rs2", "sperm2"),
-  col = c("#F7FCFD", "#E0ECF4", "#BFD3E6", "#9EBCDA", "#8C96C6", "#8C6BB1", "#88419D", "#6E016B")
-)
-
-
-covlong <- covlong %>% 
-  left_join(colours, by = "name")
-covlong$col <- paste0("'", covlong$col, "'")
-
-
-covlong %>%
-  ggplot(aes(x=value, fill=factor(name, levels=c("spg1", "spg2", "pd1", "pd2", "rs1", "rs2", "sperm1", "sperm2")))) +
-  geom_histogram(colour="black", binwidth = 2) +
-  xlim(c(0,80)) +
-  theme_bw() +
-  ggtitle("Coverage histogram") +
-  scale_fill_manual(values = c("#F7FCFD", "#E0ECF4", "#BFD3E6", "#9EBCDA", "#8C96C6", "#8C6BB1", "#88419D", "#6E016B")) +
-  facet_wrap(~factor(name, levels=c("spg1", "pd1", "rs1", "sperm1", "spg2", "pd2", "rs2", "sperm2")), nrow = 2)+
-  theme(legend.position = "none")
-
-ggsave("/path/to/out/dir/coverage_histogram_lo.count1_final_MEUv7.png", dpi = 300, height = 8, width = 20, units = "cm")
-
-
-##% CpG methylation histograms 
-png("/path/to/out/dir/Hist_perc_CpG_meth_final_MEUv7.png", res = 300, height = 6, width = 10, units = "in")
-par(mfrow = c(2, 4))
-getMethylationStats(all.samples[[1]],plot=T,both.strands = FALSE) #Histogram of % CpG methylation for sample 1 - Change [[1]] to other numbers to see other samples
-getMethylationStats(all.samples[[2]],plot=T,both.strands = FALSE)
-getMethylationStats(all.samples[[3]],plot=T,both.strands = FALSE)
-getMethylationStats(all.samples[[4]],plot=T,both.strands = FALSE)
-getMethylationStats(all.samples[[5]],plot=T,both.strands = FALSE)
-getMethylationStats(all.samples[[6]],plot=T,both.strands = FALSE)
-getMethylationStats(all.samples[[7]],plot=T,both.strands = FALSE)
-getMethylationStats(all.samples[[8]],plot=T,both.strands = FALSE)
-dev.off()
-
-##CpG coverage histograms
-png("/path/to/out/dir/Hist_CpG_cov_final_MEUv7.png", res = 300, height = 6, width = 10, units = "in")
-par(mfrow = c(2, 4))
-getCoverageStats(all.samples[[1]],plot=TRUE,both.strands = FALSE) #Histogram of CpG Coverage for sample 1
-getCoverageStats(all.samples[[2]],plot=TRUE,both.strands = FALSE)
-getCoverageStats(all.samples[[3]],plot=TRUE,both.strands = FALSE)
-getCoverageStats(all.samples[[4]],plot=TRUE,both.strands = FALSE)
-getCoverageStats(all.samples[[5]],plot=TRUE,both.strands = FALSE)
-getCoverageStats(all.samples[[6]],plot=TRUE,both.strands = FALSE)
-getCoverageStats(all.samples[[7]],plot=TRUE,both.strands = FALSE)
-getCoverageStats(all.samples[[8]],plot=TRUE,both.strands = FALSE)
-dev.off()
-
-##Sample correlation results
-getCorrelation(meth,plot=F) #scatterplot of correlation between samples
-
-####Clustering samples#
-clusterSamples(meth, dist="correlation", method="ward.D", plot=FALSE)
-clusterSamples(meth, dist="correlation", method="ward", plot=T) #plots a dendrogram
-
-png("/path/to/out/dir/Dendrogram_final_MEUv7.png", res = 300, height = 6, width = 8, units = "in")
-clusterSamples(meth, dist="correlation", method="ward", plot=T)
-dev.off()
-
-##PCA
-par(mfrow = c(1, 1))
-
-#Saving PCA
-png("/path/to/out/dir/PCA_plot_final_MEUv7.png", res = 300, height = 6, width = 8, units = "in")
-PCASamples(meth)
-dev.off()
-
-PCASamples(meth, screeplot=T) #plots screeplot
-pca <- PCASamples(meth, obj.return = T) #PCA Plot, saves as object
-
-#Better screeplot with % variance labels
-fviz_eig(pca, addlabels = T)
-
-##Saving screeplot
-png("/path/to/out/dir/Screeplot_pca_variances_final_MEUv7.png", res = 300, height = 6, width = 8, units = "in")
-fviz_eig(pca, addlabels = T) #better screeplot with % variance labels
-dev.off()
